c71e64b19cc6716c0d219870257b3239a4d4c7a5
Weston Isheim <wisheim@gmail.com>
2021-09-01 16:03:37 -0600

    Finished basic instructions

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("hello world")
+const message = document.querySelector('#message')
+
+function addMovie(event) {
+    event.preventDefault()
+    const inputField = document.querySelector('input')
+    const movie = document.createElement('li')
+    const movieTitle = document.createElement("span")
+    movieTitle.textContent = inputField.value
+    movieTitle.addEventListener("click", crossOffMovie)
+    movie.appendChild(movieTitle)
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("click", deleteMovie)
+    movie.appendChild(deleteBtn)
+    
+    const list = document.querySelector('ul')
+    list.appendChild(movie)
+    inputField.value = ""
+}
+
+document.querySelector('form').addEventListener("submit", addMovie) 
+
+function deleteMovie(event){
+    event.target.parentNode.remove()
+    message.textContent = "movie deleted"
+}
+
+function crossOffMovie(event){
+    event.target.classList.toggle("checked")
+    if(event.target.classList.contains('checked') === true){
+        message.textContent ="movie watched"
+    } else {
+        message.textContent = "movie added back"
+    }
+}
